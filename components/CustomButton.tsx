61c47fa5003151cc18a6fca01b751c82e64a04f8
ankit.nimje93@gmail.com <ankit.nimje93@gmail.com>
2023-10-28 12:30:16 -0500

    add reusable button

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+"use client";
 
-const CustomButton = () => {
+import { CustomButtonProps } from "@/types";
+
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+}: CustomButtonProps) => {
   return (
-    <div>CustomButton</div>
-  )
-}
+    <button
+      disabled={false}
+      type={"button"}
+      className={`custom-btn ${containerStyles}  `}
+      onClick={handleClick}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
+  );
+};
 
-export default CustomButton
+export default CustomButton;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,21 +1,30 @@
-import Image  from "next/image"
-import CustomButton from "./CustomButton"
+"use client";
+
+import Image from "next/image";
+import CustomButton from "./CustomButton";
 
 const Hero = () => {
+  const handleScroll = () => {};
+
   return (
     <div className="hero">
-        <div className="flex-1 pt-36 padding-x">
-            <h1 className="hero__title">
-                Find, book, and rent a car - quickly and easily!
-            </h1>
-            <p className="hero__subtitle">
-                Streamline your car rental experience with out effortless booking process.
-            </p>
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, book, and rent a car - quickly and easily!
+        </h1>
+        <p className="hero__subtitle">
+          Streamline your car rental experience with out effortless booking
+          process.
+        </p>
 
-            <CustomButton />
-        </div> 
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
